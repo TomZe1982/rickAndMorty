387e8828ef547999adc5fdc494c8bb9a9b0d7938
TomZe1982 <thomas.zeisner@gmail.com>
2021-08-05 14:14:31 +0200

    Load and hide buttons added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import './App.css'
 import Header from './components/Header'
 import CharacterGallery from './components/CharacterGallery'
 import characterResponse from './characters-response.json'
+import { useState } from 'react'
 
 function App() {
-  const characters = characterResponse.results
+  const [characters,setCharacter] = useState([])
+
+  const loadCharacters = () => {
+    setCharacter(characterResponse.results)
+  }
+
+  const hideCharacters = () => {
+    setCharacter([])
+  }
 
   return (
     <div>
       <Header title="Character Gallery" />
+      <button onClick={loadCharacters}>Load Characters</button>
+      <button onClick={hideCharacters}>Hide Characters</button>
       <CharacterGallery characters={characters} />
     </div>
   )
